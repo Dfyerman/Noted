fdb511b2870c92f161c631c7966b87f1417a0505
Dfyerman <153417503+Dfyerman@users.noreply.github.com>
2024-07-09 00:10:23 -0700

    changed some code placement

--- server.js
+++ server.js
@@ -16,16 +16,16 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/index.html'))
+  res.sendFile(path.join(__dirname, 'public/assets/index.html'))
 );
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/notes.html'))
+  res.sendFile(path.join(__dirname, 'public/assets/notes.html'))
 );
 
 app.delete('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/notes.html'))
+  res.sendFile(path.join(__dirname, 'public/assets/notes.html'))
 );
 
 
