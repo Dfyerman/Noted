fefe78c0d19f8a0e3b5e4c91b2a66d9f29be7b65
Dfyerman <153417503+Dfyerman@users.noreply.github.com>
2024-07-08 21:12:14 -0700

    added the index and html to the root

--- server.js
+++ server.js
@@ -16,16 +16,16 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, '/index.html'))
 );
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/notes.html'))
 );
 
 app.delete('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, '/notes.html'))
 );
 
 
